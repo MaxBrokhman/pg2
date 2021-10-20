3c7f741016d646bc67e99781d3bbc10a5c301b70
MaxBrohman <maksimbrochman@gmail.com>
2021-10-20 06:43:10 +0300

    view functions added

--- src/Pf.elm
+++ src/Pf.elm
@@ -7,12 +7,43 @@ import Html.Events exposing (onClick)
 import Http
 import Json.Decode as Decode exposing (Decoder, int, list, string)
 import Json.Decode.Pipeline exposing (required)
-import Pg exposing (Msg(..))
+import Pg exposing (Msg(..), urlPrefix)
 
 
 type alias Model =
     { selectedPhotoUrl : Maybe String
     }
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div
+        [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src <| urlPrefix ++ "photos/" ++ photo.url ++ "/full" ] []
+        , span [] [ text <| String.fromInt photo.size ++ "KB" ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ]
+            (List.map viewRelatedPhoto photo.relatedUrls)
+        ]
+
+
+viewRelatedPhoto : String -> Html Msg
+viewRelatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (ClickedPhoto url)
+        , src <| urlPrefix ++ "photos/" ++ url ++ "/thumb"
+        ]
+        []
 
 
 initialModel : Model
