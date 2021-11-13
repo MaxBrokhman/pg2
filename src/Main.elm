a8363de55617ab1339b9f2eb7536f4c219fa07b4
MaxBrohman <maksimbrochman@gmail.com>
2021-11-13 09:48:04 +0200

    header links added

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,55 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html exposing (Html, a, caption, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList, href)
 
 
 type alias Model =
-    {}
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | Folders
+    | NotFound
 
 
 view : Model -> Document Msg
 view model =
     { title = "Photo Groove, SPA style"
-    , body = [ text "This isn't my final form!" ]
+    , body =
+        [ viewHeader model.page
+
+        -- , content
+        , viewFooter
+        ]
     }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/gallery", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", page == targetPage ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+viewFooter : Html Msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
 
 
 type Msg
@@ -33,7 +69,7 @@ subscriptions model =
 main : Program () Model Msg
 main =
     Browser.document
-        { init = \_ -> ( {}, Cmd.none )
+        { init = \_ -> ( { page = Folders }, Cmd.none )
         , view = view
         , update = update
         , subscriptions = subscriptions
